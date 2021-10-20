292e0e603580d92c3984de475573d76b52711c43
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-10-20 17:09:09 +0300

    create a gallery using the SimpleLightbox library
    *JAVA SCRIPT*
    - Library adds image opening on click, modal opening and closing.
      scrolling through images using the keyboard
    - Add display captions to images from the alt attribute. The caption is at the bottom and appears 250 milliseconds after the image is opened.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallareRef = document.querySelector(".gallery");
+const cardsMurkup = createCardsMurkup(galleryItems);
+
+gallareRef.insertAdjacentHTML("afterbegin", cardsMurkup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createCardsMurkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>
+`;
+    })
+    .join("");
+}
