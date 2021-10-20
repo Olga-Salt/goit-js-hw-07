1c9a355f68651587b2920ef96f961944ef457fa5
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-10-20 14:57:59 +0300

    Create a gallery with the ability to click on its elements and view the full size image in a modal window.
    * Java Script*
    - Creation and rendering of markup using the "galleryItems" data array and template
    - Including the script and library styles of the basicLightbox modal window
    - Opening a modal window by clicking on a gallery item
    - Add the closing of the modal window by pressing the Escape key
    - listen to keyboard only while modal is open

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallareRef = document.querySelector(".gallery");
+const cardsMurkup = createCardsMurkup(galleryItems);
+let instance;
+
+gallareRef.insertAdjacentHTML("afterbegin", cardsMurkup);
+gallareRef.addEventListener("click", onModalOpenClick);
+
+function onModalOpenClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  gallareRef.addEventListener("keydown", onEscModalClose, { once: true });
+
+  instance = basicLightbox.create(
+    `
+    <div class="modal">
+        <img
+      src=${e.target.dataset.source}
+      alt=${e.target.alt} width="800" height="600"
+    />
+    </div>
+`
+  );
+  instance.show();
+}
+
+function createCardsMurkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onEscModalClose(evt) {
+  if (evt.code === "Escape") {
+    console.log(evt.code);
+    instance.close();
+  }
+}
+
+// ====
